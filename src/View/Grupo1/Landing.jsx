8c17e59862def0b716cecc4ffd26152cdc61ec21
ST4R-DUST <52722577+ST4R-DUST@users.noreply.github.com>
2020-06-14 00:54:08 -0400

    Grupo1: Añadida Navbar, Drawer, Pantalla Login

--- src/View/Grupo1/Landing.jsx
+++ src/View/Grupo1/Landing.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import '../../css/Grupo1/Landing.css';
 import Button from '@material-ui/core/Button';
+import NavBar from '../../Component/Grupo1/NavBar';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import history from '../../history';
 
 function Landing() {
@@ -11,13 +14,45 @@ function Landing() {
   }
 
   return (
+    <div>
+      <NavBar/>
       <div className="Body">
-        <p>
-          Vistas de Grupo 1
-        </p>
+      <Grid container spacing={3} 
+            direction="column"
+            justify="center"
+            alignItems="center">
+        <Grid item xs={12}>
+          <Paper >que shusha
+
+
+
+
+
+          </Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper >xs=6</Paper>
+        </Grid>
+        <Grid item xs={6}>
+          <Paper >xs=6</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>xs=3</Paper>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper>xs=3</Paper>
+        </Grid>
+      </Grid>
         <Button onClick={redirectHome} variant="contained" color="primary">
         Home
         </Button>
+      </div>
       </div>
   );
 }
