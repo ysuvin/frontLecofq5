651d6a5d128ab290bfb11a93c68f0ba33c65c4ef
Bastian Esteban <beps1@hotmail.com>
2020-06-27 23:25:40 -0400

    Se agrega la ruta para testear si la funcionalidad de BPeso Funciona

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,30 @@ import G1Landing from './View/Grupo1/G1Landing';
 import { Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
-	return(
-  		<div>
-    		<Switch>
-      			<Route exact path="/Home" component={Home}/>
-      			<Route exact path="/">
-        			<Redirect to ="/Home"/>
-        		</Route>     
+	return (
+		<div>
+			<Switch>
+				<Route exact path="/Home" component={Home} />
+				<Route exact path="/">
+					<Redirect to="/Home" />
+				</Route>
 
 				{/* Routing Grupo 1	*/}
-				<Route exact path="/Grupo1/" component={G1Landing}/>
-				<Route exact path="/Grupo1/*" component={G1Landing}/>	
-				{/* Fin Routing Grupo 1*/}	
+				<Route exact path="/Grupo1/" component={G1Landing} />
+				<Route exact path="/Grupo1/*" component={G1Landing} />
+				{/* Fin Routing Grupo 1*/}
 
-    		</Switch>
-    		
-  		</div>
-  );
+				{/* Routing Grupo 4    */}
+				<Route exact path="/Grupo4/adminHoras" component={AdminHoras} />
+				<Route exact path="/Grupo4">
+					<Redirect to="/Grupo4/adminHoras" />
+				</Route>
+				{/* Fin Routing Grupo 4*/}
+
+			</Switch>
+
+		</div>
+	);
 }
-  
+
 export default App;
