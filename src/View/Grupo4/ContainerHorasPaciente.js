01408e602fc66ded357a3b9cbed1e18f14e44992
Deiby C <deiby.cabrera@alumnos.uv.cl>
2020-06-29 17:40:34 -0400

    Solucion detalle vista

--- src/View/Grupo4/ContainerHorasPaciente.js
+++ src/View/Grupo4/ContainerHorasPaciente.js
@@ -1,8 +1,8 @@
 import React, {Fragment} from 'react';
 import {BrowserRouter, Route, Link} from 'react-router-dom';
-import Identify from './InscripcionHoras/Identify';
 import TemporaryDrawer from './Navbar/Navbar';
 import Box from '@material-ui/core/Box';
+import Grid from '../../Model/Grupo4/InscripcionHoras/Grids'
 import Container from '@material-ui/core/Container';
 
 const ContainerHorasPaciente = () => {
@@ -14,7 +14,7 @@ const ContainerHorasPaciente = () => {
             <div className="navbar">
                 <TemporaryDrawer />
             </div>
-            <Identify />
+            <Grid />
 
             
         </Fragment>
