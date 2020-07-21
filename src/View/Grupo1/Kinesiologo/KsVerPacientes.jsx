d32bc554982db1dd8ddcb81ac1015de1dc9f57bb
Manuel Rojas <manuel.rojas@alumos.uv.cl>
2020-07-20 20:07:34 -0400

    Home y ficha, boton ver ficha agregado

--- src/View/Grupo1/Kinesiologo/KsVerPacientes.jsx
+++ src/View/Grupo1/Kinesiologo/KsVerPacientes.jsx
@@ -8,6 +8,15 @@ import Pacientes from '../../../Model/Grupo1/Pacientes';
 import Button from '@material-ui/core/Button';
 import history from '../../../history.jsx';
 
+import logo from '../../../logo.png';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { useTheme } from '@material-ui/core/styles';
+
 function KsVerPacientes() {
   
 
@@ -18,9 +27,25 @@ function KsVerPacientes() {
       history.push('/Grupo1/KsAsFecha')
     }
 
+    const [open, setOpen] = React.useState(false);
+    const theme = useTheme();
+    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
+
+    const handleClickOpen = () => {
+      setOpen(true);
+    };
+
+    const handleClose = () => {
+      setOpen(false);
+    };
+
   return (
     <div className="wrapper">
+      <div className="header">
+      
+      </div>
       <div className="body">
+      <img src={logo} className="App-logo" alt="logo" />
       Seleccionar Paciente:
       <Autocomplete
         value={paciente}
@@ -49,12 +74,39 @@ function KsVerPacientes() {
             </Paper>
             </Grid>
             <Grid item xs={12}>
-            <Paper className="paper">
+            {/* <Paper className="paper">
             <p>Nombre: {`${paciente !== null ? `${paciente.nombre}` : ''}`}</p>
             <p>Edad: {` ${paciente !== null ? `${paciente.edad}` : ''}`}</p>
             <p>Peso: {` ${paciente !== null ? `${paciente.peso}` : ''}`}</p>
             <p>Estatura: {` ${paciente !== null ? `${paciente.estatura}` : ''}`}</p>
-            </Paper>
+            </Paper> */}
+            <Button variant="contained" color="primary" onClick={handleClickOpen}>
+              Ver detalle
+            </Button>
+            <Dialog
+              fullScreen={fullScreen}
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="responsive-dialog-title"
+            >
+            <DialogTitle id="responsive-dialog-title">{"Ficha del paciente"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                <p>Nombre: {`${paciente !== null ? `${paciente.nombre}` : ''}`}</p>
+                <p>Edad: {` ${paciente !== null ? `${paciente.edad}` : ''}`}</p>
+                <p>Peso: {` ${paciente !== null ? `${paciente.peso}` : ''}`}</p>
+                <p>Estatura: {` ${paciente !== null ? `${paciente.estatura}` : ''}`}</p>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              {/* <Button autoFocus onClick={handleClose} color="primary">
+                Disagree
+              </Button> */}
+              <Button onClick={handleClose} color="primary" autoFocus>
+                Listo
+              </Button>
+            </DialogActions>
+            </Dialog>
           </Grid>
           <Grid item xs={12}>
             {paciente !== null
